f7c5b001e124123ba6a2fac809593c01171f993e
mkhizes <lubanzi01trading@gmail.com>
2024-05-02 07:25:30 +0200

    sorting out files and folders accordingly

--- week02/scripts/hamburgermenu.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('_______', () => {
-	navigation._______.toggle('_______');
-	hamButton.classList.toggle('open');
-});
--- week02/scripts/temples.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const currentYear = new Date().getFullYear();
-    const lastModifiedDate = new Date(document.lastModified);
-    
-    const yearElement = document.getElementById('copyright');
-    yearElement.textContent = `© ${currentYear} - Last Modified: ${lastModifiedDate.toLocaleDateString()}`;
-  });
